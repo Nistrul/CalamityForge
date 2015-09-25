181650fd8c9093bb069dcf48c8827e343dae5c15
Dale Freya <dale.freya@gmail.com>
2015-09-26 05:41:12 +1000

    Updating address bar on ajax. Back button goes back to previous queries.

--- public/js/main.js
+++ public/js/main.js
@@ -6,9 +6,24 @@ $(function() {
 		{
 			if (e.which == 13) 
 			{
+				var $target = $('#target');
 				$.get('/encounterTable/xp/' + $xpinput.val(), {}, function(result) {
-					$('#target').html(result);
+					$target.html(result);
+					window.history.pushState(
+						{
+							"targetHtml":$target.html()
+						}, 
+						'', 
+						'/encounters/xp/' + $xpinput.val());
+					console.log("pushed it");
 				});
 			}
 		});
 });
+
+window.onpopstate = function(e){
+    if(e.state){
+        $('#target').html(e.state.targetHtml);
+        $('#xpinput').val('');
+    }
+};
--- routes/encountersView.js
+++ routes/encountersView.js
@@ -3,11 +3,24 @@
 
 var express = require('express');
 var router = express.Router();
+var encounterTable = require('../src/encounterTable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) 
 {
 	res.render('encounters', { title: 'Encounters' });
+});
+
+router.get('/xp/:xpvalue', function(req, res, next)
+{
+	var xp = parseInt(req.params.xpvalue);
+
+	var table = encounterTable.create(xp, 100);
+	var selected = encounterTable.select(table);
+
+	console.log('selected: ' + JSON.stringify(selected, null, 4));
+
+	res.render('encountersXP', { title: 'Encounters', encounters: table, chosen: selected});
 });
 
 module.exports = router;
