660d1aba5dd9f4afd5f48cb920d543429562d92c
Dale Freya <dale.freya@gmail.com>
2015-09-27 23:24:00 +1000

    Fixed up error handling for TableView route.

--- routes/encounterTableView.js
+++ routes/encounterTableView.js
@@ -9,11 +9,27 @@ router.get('/xp/:xpvalue', function(req, res, next)
 {
 	var xp = parseInt(req.params.xpvalue);
 
-	var table = encounterTable.create(xp, 100);
-	var selected = encounterTable.select(table);
+	if (isNaN(xp))
+	{
+		res.status(400);
+		res.render('errorInner', { error: { message: 'xp must be an integer', status: 400 }});
+	}
+	else if (xp < encounterTable.constants.xpmin || xp > encounterTable.constants.xpmax)
+	{
+		res.status(400);
+		res.render('errorInner', { 
+			error: { 
+				message: 'xp must be >= ' + encounterTable.constants.xpmin + ' and <= ' + encounterTable.constants.xpmax, 
+				status: 400
+			}
+		});
+	}
+	else
+	{
+		var table = encounterTable.create(xp, 100);
 
-
-	res.render('encounterTable', { title: 'Encounters', encounters: table, chosen: selected});
+		res.render('encounterTable', { title: 'Encounters', encounters: table});
+	}
 });
 
 module.exports = router;
